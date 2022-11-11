3a547e9b992f711a6ea09fdb63b0f4d394cc45a4
Jonatan008Repo <vergara_jona@hotmail.com>
2022-11-11 16:27:25 -0600

    add event load

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-function seleccionarMascotaJugador() {
-  alert("hola");
+function iniciarJuego() {
+  let botonMascota = document.getElementById('boton-mascota');
+  botonMascota.addEventListener('click', seleccionarMascotaJugador);
 }
+function seleccionarMascotaJugador() {
+  let inputHipodoge = document.getElementById('hipodoge');
+  let inputCapipepo = document.getElementById('capipepo');
+  let inputRatigueya = document.getElementById('ratiguey');
+  let inputLangostelvis = document.getElementById('langoste');
+  let inputTucapalma = document.getElementById('tucapalm');
+  let inputPydos = document.getElementById('pydos');
 
-let botonMascota = document.getElementById('boton-mascota');
+  if (inputHipodoge.checked) {
+    console.log('Seleccionaste a Hipodoge    ')
+  } else if (inputCapipepo.checked) {
+    console.log('Seleccionaste a Capipepo    ')
+  } else if (inputRatigueya.checked) {
+    console.log('Seleccionaste a Ratigueya   ')
+  } else if (inputLangostelvis.checked) {
+    console.log('Seleccionaste a Langostelvis')
+  } else if (inputTucapalma.checked) {
+    console.log('Seleccionaste a Tucapalma   ')
+  } else if (inputPydos.checked) {
+    console.log('Seleccionaste a Pydos    ')
+  } else {
+    console.log('Selecciona una mascota')
+  }
 
-botonMascota.addEventListener('click', seleccionarMascotaJugador);
+
+
+}
+window.addEventListener('load', iniciarJuego);
